c4832762746576c0efd90bbe5abe536b366f6b21
jijibaebaeeee <isnna1@naver.com>
2023-12-14 02:04:08 +0900

    마무리 고생했어요.

--- bigdb/Insert_emd_areas.py
+++ bigdb/Insert_emd_areas.py
@@ -13,15 +13,14 @@ conn = pymysql.connect(host=db['host'], port=db['port'],
 
 try:
     with conn.cursor() as cursor:
-        # 기존 데이터를 지우는 SQL문
+      
         cursor.execute("DELETE FROM emd_areas")
 
-        # 시군구 코드와 읍면동 이름을 매핑하여 INSERT 문 생성
         for idx, emd_row in emd_df.iterrows():
-            # 시군구명에 해당하는 시군구 코드 찾기
+            
             matching_sigg = sigg_df[sigg_df['시군구'] == emd_row['시군구명']]
             
-            # 일치하는 행이 있는 경우에만 INSERT 문 실행
+            
             if not matching_sigg.empty:
                 sigg_code = matching_sigg['시군구코드'].iloc[0]
                 sql = "INSERT INTO `emd_areas` (`id`, `sigg_area_id`, `name`) VALUES (%s, %s, %s)"
--- bigdb/Insert_sigg_areas.py
+++ bigdb/Insert_sigg_areas.py
@@ -13,19 +13,18 @@ conn = pymysql.connect(host=db['host'], port=db['port'],
 
 try:
     with conn.cursor() as cursor:
-        # 기존 데이터를 지우는 SQL문 (테이블을 새로운 데이터로 재설정하려는 경우)
+        
         cursor.execute("DELETE FROM sigg_areas")
 
-        # 시도 코드의 앞 두 자리를 기반으로 시군구 코드를 매핑하고 INSERT 문 생성
         for idx, row in df.iterrows():
-            # 시도 코드의 앞 두 자리 추출
-            sido_code_prefix = str(row['코드'])[:2]
-
-            # 해당 시도 코드에 해당하는 시군구 데이터 찾기
-            matching_sigg = df2[df2['시군구코드'].astype(str).str.startswith(sido_code_prefix)]
             
-            # 각 시군구 데이터에 대해 INSERT 문 실행
-            for _, sigg_row in matching_sigg.iterrows():
+            sido_code = str(row['코드'])[:2]
+
+            
+            sigg = df2[df2['시군구코드'].astype(str).str.startswith(sido_code)]
+            
+            
+            for _, sigg_row in sigg.iterrows():
                 sql = "INSERT INTO `sigg_areas` (`id`, `sido_area_id`, `name`) VALUES (%s, %s, %s)"
                 cursor.execute(sql, (sigg_row['시군구코드'], row['코드'], sigg_row['시도_시군구']))
 
@@ -33,3 +32,5 @@ try:
     conn.commit()
 finally:
     conn.close()  # 데이터베이스 연결 닫기
+
+    
